32e89a2c905a5adafe044a6db84c5ab90929ecb8
mviencek <32376631+mviencek@users.noreply.github.com>
2018-11-08 13:12:21 -0800

    adding is palindrome to example.  didnt feel the need to create a new project

--- FizzBuzzExample/Program.cs
+++ FizzBuzzExample/Program.cs
@@ -27,12 +27,50 @@ namespace FizzBuzzExample
             }            
             Console.WriteLine("");
         }
+
+        //check for palindrome regardless of case or trailing/leading spaces.
+        //trim string and treat prior to comparing
+        public static bool IsPalindrome(String inputString) {
+            bool palin = true;
+            if (string.IsNullOrEmpty(inputString) || string.IsNullOrEmpty(inputString.Trim())) {
+                return false;
+            }
+            if (inputString.Trim().Length == 1) {
+                return palin;
+            }
+            String treatedInput = inputString.ToUpper().Trim();
+            int r = treatedInput.Trim().Length - 1;
+            for (int i = 0; i < inputString.Length / 2; i++)
+            {
+                char left = treatedInput[i];
+                char right = treatedInput[r];
+                if (left.CompareTo(right) != 0)
+                {
+                    return false;
+                }
+                r--;
+            }
+            return palin;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("FizzBuzz test ---------------------------------------------------------");
+            Console.WriteLine("");
             for (int i = 1; i <= 100; i++) {
-                ParseFizzBuzz(i);
-            }
-       
+                 ParseFizzBuzz(i);
+             }
+
+            Console.WriteLine("");
+            Console.WriteLine("Palindrome test ---------------------------------------------------------");
+            Console.WriteLine("");
+            String s = null;
+            Console.WriteLine("abcdcba is a palindrome: " + IsPalindrome("abcdcba")); //should be true
+            Console.WriteLine("abddcba is a palindrome: " + IsPalindrome("abddcba")); //should be false
+            Console.WriteLine("abCdcbA is a palindrome: " + IsPalindrome("abCdcbA")); //should be true
+            Console.WriteLine("empty string is a palindrome: " + IsPalindrome("  "));  //should be false
+            Console.WriteLine("null string is a palindrome: " + IsPalindrome(s)); //should be false
+            Console.WriteLine("a is a palindrome: " + IsPalindrome("a")); //should be true
+
         }
     }
 }
